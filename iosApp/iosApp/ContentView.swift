f03f096ac6dad82da9d0b6be12b34acd5fc82183
Rodrigo <rodrigocavalcante.pro@gmail.com>
2024-02-20 16:11:32 -0300

    [shared-module] - creating iosApp

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Rodrigo Cavalcante on 20/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
